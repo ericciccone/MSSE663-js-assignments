e0049f583724852428ced53aa44dc235c9c85d90
ericciccone <75946180+ericciccone@users.noreply.github.com>
2021-05-08 10:56:04 -0600

    Add files via upload

---
+++ week1/server.js
@@ -0,0 +1,14 @@
+var hello1 = 'Hello World';
+const hello2 = 'Hello Worlds!!!';
+
+hello1 = 'derp';
+hello2 = 'derp2';
+
+console.log(hello1);
+console.log(hello2);
+
+function hello() {
+    console.log('Hello Words!');
+}
+
+hello();
