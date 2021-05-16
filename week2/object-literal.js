a915733dfcd6cfabbe117e293b095498a654e41d
ericciccone <75946180+ericciccone@users.noreply.github.com>
2021-05-15 19:53:31 -0600

    Update object-literal.js

--- week2/object-literal.js
+++ week2/object-literal.js
@@ -6,3 +6,37 @@
  * #5 Convert the object literal to an Class
  * #6 Then call its method and output to the console.
  */
+
+var monkey = {
+    name:'Japanese Macaque',
+    nickname: 'Snow monkey',
+    hangout: 'hot springs',
+    formalizer: function() {
+        return "A " + this.name + " is also known as a " + this.nickname;
+    },
+    hanging: function() {
+        return "The " + this.nickname + " hangs out in " + this.hangout;
+    }
+};
+
+console.log(monkey.formalizer());
+console.log(monkey.hanging());
+
+class Monkey {
+    constructor(name, nickname, hangout) {
+        this.name = name;
+        this.nickname = nickname;
+        this.hangout = hangout;
+    }
+    formalize() {
+        return "A " + this.name + " is also known as a " + this.nickname;
+    }
+    hang() {
+        return "The " + this.nickname + " hangs out in " + this.hangout;
+    }
+
+}
+
+let kozaru = new Monkey('Japanese Macaque', 'Snow monkey', 'hotsprings');
+console.log(kozaru.formalize());
+console.log(kozaru.hang());
