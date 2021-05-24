53f38bc7868c90c2f12550f0bd52d9c60e3e12d0
ericciccone <75946180+ericciccone@users.noreply.github.com>
2021-05-23 22:28:16 -0600

    Update app.spec.ts

--- week3/src/app.spec.ts
+++ week3/src/app.spec.ts
@@ -1,2 +1,19 @@
-import { expect } from 'chai';
 import 'mocha';
+import { expect } from 'chai';
+import { Bengal } from './app';
+
+describe('Test for app.ts', () => {
+    const bengal = new Bengal("rosettes", "sparrow", 6);
+
+    it('should output Cat\'s name', () => {
+            expect(bengal.name).to.equal('sparrow');
+    })
+
+    it('should output Cat\'s age', () => {
+        expect(bengal.age).to.equal(6);
+    })
+
+    it('shoud output Cat\'s markings', () => {
+        expect(bengal.markings).to.equal("rosettes");
+    })
+})
