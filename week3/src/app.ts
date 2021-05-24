ece75ed9bc329cebb8304e97da1a8c3af28881c2
ericciccone <75946180+ericciccone@users.noreply.github.com>
2021-05-23 22:28:41 -0600

    Update app.ts

--- week3/src/app.ts
+++ week3/src/app.ts
@@ -1,1 +1,33 @@
-// Source code here
+export interface Cat {
+    markings: string;
+    name: string;
+    age: number;
+    getName: () => string;
+    getAge: () => number;
+//    isComplete: () => boolean;
+}
+
+export class Bengal implements Cat {
+    constructor (public markings: string, public name: string, public age: number) {
+        this.markings = markings; 
+        this.name = name; 
+        this.age = age;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    getAge(): number {
+        return this.age;
+    }
+
+//    isComplete() {
+//        if (!(this.markings && this.name && this.age)) { 
+//            return true;}
+//    }
+}
+
+const sparrow = new Bengal("rosettes", "sparrow", 7);
+console.log(sparrow.getName());
+console.log(sparrow.getAge());
