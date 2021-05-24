52b32a8a499b5e84eee2e12862335afbded6a282
ericciccone <75946180+ericciccone@users.noreply.github.com>
2021-05-23 22:29:32 -0600

    Update observable.ts

--- week3/src/observable.ts
+++ week3/src/observable.ts
@@ -1,1 +1,5 @@
-// Source code here
+ngOnInit() {
+    const array=[1,3,5,7,9]
+    const observables = of(array);
+    observables.subscribe(values => console.log(values), () => console.log("...odds streamed")) 
+  }
