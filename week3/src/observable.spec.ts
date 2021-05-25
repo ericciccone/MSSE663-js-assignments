a1e4f8af43b23f97847f562ffbb48363e492cdac
ericciccone <75946180+ericciccone@users.noreply.github.com>
2021-05-24 22:05:43 -0600

    Update observable.spec.ts

--- week3/src/observable.spec.ts
+++ week3/src/observable.spec.ts
@@ -1,2 +1,9 @@
-import { expect } from 'chai';
 import 'mocha';
+import { expect } from 'chai';
+import { printArray } from './observables';
+
+describe('Test for app.ts', () => {
+    it('should output the array 1 3 5 7 9', () => {
+            expect(printArray.subscribe(console.log)).to.equal([1,3,5,7,9]);
+    })
+})
