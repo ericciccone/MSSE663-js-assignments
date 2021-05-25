356f93ed81b89b131d5586aefc6a6f3b4065957f
ericciccone <75946180+ericciccone@users.noreply.github.com>
2021-05-24 21:41:40 -0600

    Update observable.ts

--- week3/src/observable.ts
+++ week3/src/observable.ts
@@ -1,3 +1,12 @@
-    const array=[1,3,5,7,9]
-    const observables = of(array);
-    observables.subscribe(values => console.log(values), () => console.log("...odds streamed")) 
+//1 Create an observable stream of an array (utilizing the of keyword).
+
+//2 Map over the values in the array.
+
+//3 Subscribe and ouput values to the concole.
+
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+    const array = of([1,3,5,7,9]);
+    const printArray = array.pipe(map(values => values));
+    printArray.subscribe(console.log);
