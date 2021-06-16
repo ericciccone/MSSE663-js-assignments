4567a0d7cd98ab511f0efbddda1aa2729f0d301d
ericciccone <75946180+ericciccone@users.noreply.github.com>
2021-06-15 18:34:33 -0600

    Update currying.ts

--- week6/src/currying.ts
+++ week6/src/currying.ts
@@ -27,6 +27,11 @@ export function hoistMath(a: number, b: number, c: number) {
 }
 console.log(hoistMath(1,2,3))
 
+export const subtractor = (a: number) => (b: number) => (c: number) => a + b -c;
+export const adder = (a: number) => (b: number) => (c: number) => subtractor(a)(b)(c);
+
+export const mathHoist = (a: number) => (b: number) => (c: number) => adder(a)(b)(c);
+
 // #3 Write a curried function that returns an array containing the ninjas who have a black belt
 export const ninjasOne = [
   { name: 'Michelangelo', belt: 'white' },
@@ -89,3 +94,58 @@ export const ninjasWithStatus = (ninjas: any) => {
 }
 
 console.log(objectsGroup(ninjasOne)(ninjasTwo))
+
+//or
+
+interface Ninja {
+  name: string;
+  belt: string;
+}
+
+export const ninjas1: Ninja[] = [
+  { name: 'Michelangelo', belt: 'white' },
+  { name: 'Donatello', belt: 'green' },
+  { name: 'Raphael', belt: 'black' },
+  { name: 'Leonardo', belt: 'black' },
+  { name: 'Mr. Miyagi', belt: 'black' },
+  { name: 'Rocky', belt: 'black' },
+  { name: 'Colt', belt: 'green' },
+  { name: 'Tum Tum', belt: 'white' },
+  { name: 'Haru', belt: 'white' },
+  { name: 'The Bride', belt: 'black' },
+  { name: 'Cammy', belt: 'black' },
+  { name: 'Wong Fei-hung', belt: 'green' }
+];
+
+export const ninjas2: Ninja[] = [
+  { name: 'Michelangelo', belt: 'white' },
+  { name: 'Donatello', belt: 'green' },
+  { name: 'Raphael', belt: 'black' },
+  { name: 'Jim', belt: 'black' },
+  { name: 'The Rat', belt: 'black' }
+];
+
+export const bb: Ninja[] = [...ninjas1, ...ninjas2].filter( ninja => ninja.belt ==='black');
+
+type Object = { [key: string]: string };
+
+interface ninjaInterface extends Ninja {
+  status: string;
+}
+
+constant statuses: Object = {
+  white: 'grasshopper',
+  green: 'warrior',
+  black: 'sensei'
+};
+
+const mapper = (arr: Ninja[]) => (status: Object) =>
+  arr.map((ninja: Ninja) => {
+  return {
+    ...ninja,
+    status: status[ninja.belt]
+};
+});
+
+export const ninjaBB1: NinjaBB[] = mapper(ninjas1)(statuses);
+export const ninjaBB2: NinjaBB[] = mapper(ninjas2)(statuses);
